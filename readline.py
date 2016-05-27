a8184346f9ce39a11cd47b4e8bbbfd3e33980ad9
Jaehwna-Kang <lupang1085@hanmail.net>
2016-05-27 12:46:39 +0900

    no message

---
+++ readline.py
@@ -0,0 +1,5 @@
+# -*- coding:utf8 -*-
+f = open("새파일.txt", 'r')
+line = f.readline()
+print(line)
+f.close()
---
+++ sys1.py
@@ -0,0 +1,5 @@
+# -*- coding:utf8 -*-
+import sys
+args = sys.argv[1:]
+for i in args:
+    print(i)
