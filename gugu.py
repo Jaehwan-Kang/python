477147143c6859328c71a5db161430c82cf3a60a
Jaehwna-Kang <lupang1085@hanmail.net>
2016-05-30 14:47:32 +0900

    no message

--- gugu.py
+++ gugu.py
@@ -1,16 +1,9 @@
 # -*- coding:utf8 -*-
 def GuGu(n):
-
-    result = []
-    result.append(n*1)
-    result.append(n*2)
-    result.append(n*3)
-    result.append(n*4)
-    result.append(n*5)
-    result.append(n*6)
-    result.append(n*7)
-    result.append(n*8)
-    result.append(n*9)
+    i = 1
+    while i < 10:
+        result.append(n * i)
+        i = i + 1
     return result
 
 print(GuGu(2))
