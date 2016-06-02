7347db1d1bcca745a62d6e36a9a2f030705acb35
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-02 11:47:01 +0900

    rename check_haproxy_stat

---
+++ check_haproxy_stat.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#
+# Usage
+# > python check_haproxy_stat.py [URL] [USER] [PASSWORD] [LISTENER]
+#
+# Example
+# > python check_haproxy_stat.py 211.61.155.206:1936 cyebiz Cyebizadmin151 lg-homeboy01
+import sys
+from haproxystats import HAProxyServer
+
+haproxy = HAProxyServer(sys.argv[1], sys.argv[2], sys.argv[3], timeout=3)
+
+listner = {}
+
+for b in haproxy.listeners:
+    listner[b.name] = b.status
+
+try:
+    if sys.argv[4] in listner:
+
+        if listner[sys.argv[4]] == "UP":
+            print("%s : %s" % (sys.argv[4], listner[sys.argv[4]]))
+            sys.exit(0)  # OK exit code
+        elif listner[sys.argv[4]] == "DOWN":
+            print("%s : %s" % (sys.argv[4], listner[sys.argv[4]]))
+            sys.exit(2)  # Critical exit code
+        else:
+            print("UNKNOWN %s : %s" % (sys.argv[4], listner[sys.argv[4]]))
+            sys.exit(3)  # Unknown exit code
+
+    else:
+        print("Not Matched Listner : %s" % sys.argv[4])
+        sys.exit(3) # Unknown exit code
+
+except IndexError as e:
+    print("Not Matched Arguments")
+    sys.exit(3) # Unknown exit code
