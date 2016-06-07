8cd451abcf3ef355bf9e31cd83de57ab431bbd0c
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-07 16:48:56 +0900

    no message

--- check_mysql_query.py
+++ check_mysql_query.py
@@ -7,8 +7,6 @@ import sys
 conn = pymysql.connect(host='db.sumradio.com', port=3306, user='root', passwd='cy.admin.235', db='information_schema')
 cur = conn.cursor()
 result = cur.execute("show status like 'Threads_connected'")
-cur.close()
-conn.close()
 
 if result > sys.argv[1] and result < sys.argv[2]:
     print("Warning Connections %s" % result)
@@ -18,3 +16,7 @@ elif result > sys.argv[2]:
     sys.exit(2)
 else:
     print("O.K Connections %s" % result)
+    
+cur.close()
+conn.close()
+
