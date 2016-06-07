24a2bdefa7abea2e240cabfcb7c5dde7036dd6ea
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-07 16:57:42 +0900

    no message

--- check_mysql_query.py
+++ check_mysql_query.py
@@ -9,14 +9,14 @@ cur = conn.cursor()
 cur.execute("show status like 'Threads_connected'")
 
 for row in cur:
-    if row > sys.argv[1] and row < sys.argv[2]:
-    print("Warning Connections %s" % row)
+    if row[1] > sys.argv[1] and row[1] < sys.argv[2]:
+    print("Warning Connections %s" % row[1])
     sys.exit(1)
 elif row > sys.argv[2]:
-    print("Critical Connectios %s" % row)
+    print("Critical Connectios %s" % row[1])
     sys.exit(2)
 else:
-    print("O.K Connections %s" % row)
+    print("O.K Connections %s" % row[1])
 
 cur.close()
 conn.close()
