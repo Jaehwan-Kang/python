a55de66eeabf9806e070ef1224be803d336a4116
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-07 16:27:13 +0900

    no message

--- check_mysql_query.py
+++ check_mysql_query.py
@@ -4,7 +4,7 @@
 import pymysql
 import sys
 
-conn = pymysql.connect(host='db.sumradio.com', port=3306, user='root', passwd='cy.admin.235')
+conn = pymysql.connect(host='db.sumradio.com', port=3306, user='root', passwd='cy.admin.235', db='information_schema')
 
 cur = conn.cursor()
 
