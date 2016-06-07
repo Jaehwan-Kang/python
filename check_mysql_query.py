3056588fa73ed73ebf74d201f4aab4b963a5675d
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-07 16:45:15 +0900

    no message

--- check_mysql_query.py
+++ check_mysql_query.py
@@ -5,14 +5,16 @@ import pymysql
 import sys
 
 conn = pymysql.connect(host='db.sumradio.com', port=3306, user='root', passwd='cy.admin.235', db='information_schema')
-
 cur = conn.cursor()
-
 result = cur.execute("show status like 'Threads_connected'")
-
-print(result[1])
-#for row in cur:
-#    print(row[1])
-
 cur.close()
 conn.close()
+
+if sys.argv[1] < result < sys.argv[2]:
+    print("Warning Connections %s" % result)
+    sys.exit(1)
+elif result > sys.argv[2]:
+    print("Critical Connectios %s" % result)
+    sys.exit(2)
+else:
+    print("O.K Connections %s" % result)
