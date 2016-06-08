d60eb517bfc1d2eea6e710c44e840b68fb31b71e
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-08 15:15:18 +0900

    no message

--- check_mysql_query.py
+++ check_mysql_query.py
@@ -3,9 +3,11 @@
 
 import pymysql, sys
 
-conn = pymysql.connect(host=sys.argv[1], port=3306, user=sys.argv[2], passwd=sys.argv[3], db='information_schema')
+conn = pymysql.connect(host=sys.argv[1], port=3306, user=sys.argv[2], passwd=sys.argv[3], db='mysql')
 cur = conn.cursor()
-cur.execute("select count(ID) from PROCESSLIST")
+
+cur.execute("show status like 'Threads_connected'")
+#cur.execute("select count(ID) from PROCESSLIST")
 
 for row in cur:
     if int(row[1]) >= int(sys.argv[4]) and int(row[1]) < int(sys.argv[5]):
