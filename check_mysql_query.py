290f5e68e923a3d98e1d7a8b49c49e4e278900d6
Jaehwna-Kang <lupang1085@hanmail.net>
2016-06-08 15:18:20 +0900

    no message

--- check_mysql_query.py
+++ check_mysql_query.py
@@ -6,22 +6,22 @@ import pymysql, sys
 conn = pymysql.connect(host=sys.argv[1], port=3306, user=sys.argv[2], passwd=sys.argv[3], db='mysql')
 cur = conn.cursor()
 
-cur.execute("show status like 'Threads_connected'")
-#cur.execute("select count(ID) from PROCESSLIST")
+#cur.execute("show status like 'Threads_connected'")
+cur.execute("select count(ID) from PROCESSLIST")
 
 for row in cur:
-    if int(row[1]) >= int(sys.argv[4]) and int(row[1]) < int(sys.argv[5]):
-        print("Warning Connections %s" % int(row[1]))
+    if int(row[0]) >= int(sys.argv[4]) and int(row[0]) < int(sys.argv[5]):
+        print("Warning Connections %s" % int(row[0]))
         cur.close()
         conn.close()
         sys.exit(1)
-    elif int(row[1]) >= int(sys.argv[5]):
-        print("Critical Connectios %s" % row[1])
+    elif int(row[0]) >= int(sys.argv[5]):
+        print("Critical Connectios %s" % int(row[0]))
         cur.close()
         conn.close()
         sys.exit(2)
     else:
-        print("O.K Connections %s" % row[1])
+        print("O.K Connections %s" % int(row[0]))
         cur.close()
         conn.close()
         sys.exit(0)
